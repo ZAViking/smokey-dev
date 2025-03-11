458889705a45b9b9a3dfd7fa2a6280d1281daaf0
ZA Viking <78074905+ZAViking@users.noreply.github.com>
2025-03-12 00:50:36 +0200

    Update index.vue

--- pages/index.vue
+++ pages/index.vue
@@ -13,7 +13,7 @@ const categories = [
         links: [
           { name: "Github", url: "https://github.com/ZAViking" },
           { name: "MyNinja", url: "https://myninja.ai/" },
-          { name: "Python", url: "/coding/python" },
+          { name: "SVGL", url: "https://svgl.app/" },
           { name: "Vue.js", url: "/coding/vue" },
           { name: "Java: Project for beginners", url:"https://www.freecodecamp.org/news/javascript-projects-for-beginners/?ref=dailydev#heading-how-to-create-a-review-carousel" },
           { name: "Automate code commenting", url:"https://blog.logrocket.com/automate-code-commenting-using-vs-code-ollama/?ref=dailydev" },
